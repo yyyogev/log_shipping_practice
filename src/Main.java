01ef3d870e9e7c9517542c2f4cabcdf3fbe85945
yyyogev <yogev.metzuyanim@logz.io>
2018-12-27 18:19:09 +0200

    upload all

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import static java.lang.Thread.sleep;
+
+//import org.apache.logging.log4j.ThreadContext;
+public class Main {
+
+
+    public static void main(String[] args) {
+        Logger logger = LogManager.getLogger(Main.class);
+        System.out.println("Hello World!");
+        logger.info("Testing logz.io!");
+        logger.warn("Winter is coming");
+        for (int i = 100000; i < 200000; i++) {
+            if (isPrime(i)) {
+                try {
+                    sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                logger.info("found prime number " + i);
+            }
+        }
+    }
+
+    private static boolean isPrime(int num)
+    {
+        if (num == 2)
+            return true;
+        if (num < 2 || num % 2 == 0)
+            return false;
+        for (int i = 3; i * i <= num; i += 2)
+            if (num % i == 0)
+                return false;
+        return true;
+    }
+}
